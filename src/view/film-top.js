66af983934c07f9595f3aaa4f1276f1492ffee97
Aleksandr Kurilenko <kurilchnaprimer@gmail.com>
2020-11-28 17:20:55 +0300

    Modules

---
+++ src/view/film-top.js
@@ -0,0 +1,17 @@
+import { createFilmCardTemplate } from "./film-card";
+
+const createCardsForTop = (count) => {
+  let result = ``;
+  for (let x = 0; x < count; x++) {
+    result += createFilmCardTemplate();
+  }
+  return result;
+};
+
+export const createFilmTop = (count, title) => {
+  return `<section class="films-list films-list--extra">
+    <h2 class="films-list__title">${title}</h2>
+    <div class="films-list__container">${createCardsForTop(count)}
+    </div>
+  </section>`;
+};
