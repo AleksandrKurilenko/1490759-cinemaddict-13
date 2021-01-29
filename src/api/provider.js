90ccec36a7206c6d5553231aaa30290b6e66301a
AleksandrKurilenko <kurilchnaprimer@gmail.com>
2021-01-29 14:35:47 +0300

    preparation

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,4 +1,19 @@
-import {isOnline, renderToast} from '../utils';
+const SHOW_TIME = 4000;
+const isOnline = () => {
+  return window.navigator.onLine;
+};
+
+export const renderToast = (message) => {
+  const toast = document.createElement(`div`);
+  toast.textContent = message;
+  toast.classList.add(`toast`);
+
+  document.body.append(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, SHOW_TIME);
+};
 
 const createStoreStructure = (items) => {
   return items.reduce((acc, current) => {
