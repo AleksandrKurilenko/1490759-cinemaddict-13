455342dc04b574580fa731a809a429804c5c49ef
AleksandrKurilenko <kurilchnaprimer@gmail.com>
2021-01-29 23:05:48 +0300

    offline

--- src/data.js
+++ src/data.js
@@ -72,13 +72,3 @@ export const UpdateType = {
   MINOR: `MINOR`,
   MAJOR: `MAJOR`
 };
-
-// const POSTERS = [
-//   `made-for-each-other.png`,
-//   `popeye-meets-sinbad.png`,
-//   `sagebrush-trail.jpg`,
-//   `santa-claus-conquers-the-martians.jpg`,
-//   `the-dance-of-life.jpg`,
-//   `the-great-flamarion.jpg`,
-//   `the-man-with-the-golden-arm.jpg`,
-// ];
