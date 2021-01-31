416d3b9efd27a15c1bced8a0833d6b098cd0f2a8
AleksandrKurilenko <kurilchnaprimer@gmail.com>
2021-01-31 22:37:25 +0300

    исправление замечаний4

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import Abstract from './view/abstract';
 import {Category, RenderPosition} from './data';
+import dayjs from 'dayjs';
 
 const SHOW_TIME = 4000;
 
@@ -88,4 +89,52 @@ export const getDuration = (duration) => {
 
 export const isOnline = () => {
   return window.navigator.onLine;
+};
+
+export const getFormattedTime = (date) => {
+
+  const difference = (+new Date() - +new Date(date)) / 60000;
+
+  const hoursDiff = difference / 60;
+
+  const daysDiff = hoursDiff / 24;
+
+  const monthDiff = daysDiff / 30;
+
+  if (difference < 1) {
+    return `now`;
+
+  } else if (difference >= 1 && difference < 5) {
+    return `a few minutes ago`;
+
+  } else if (difference >= 5 && difference < 60) {
+    return `${Math.floor(difference)} minutes ago`;
+
+  } else if (difference > 60 && difference < 61) {
+    return `1 hour ago`;
+
+  } else if (difference >= 61 && hoursDiff < 24) {
+    return `${Math.floor(hoursDiff)} hours ago`;
+
+  } else if (hoursDiff > 24 && hoursDiff < 48) {
+    return `1 day ago`;
+
+  } else if (hoursDiff >= 48 && daysDiff < 30) {
+    return `${Math.floor(daysDiff)} days ago`;
+
+  } else if (daysDiff >= 30 && daysDiff < 60) {
+    return `1 month ago`;
+
+  } else if (daysDiff >= 60 && monthDiff < 12) {
+    return `${Math.floor(monthDiff)} month ago`;
+
+  } else if (monthDiff >= 12 && monthDiff < 24) {
+    return `1 year ago`;
+
+  } else if (monthDiff > 24) {
+    return `${Math.floor(monthDiff / 12)} years ago`;
+
+  } else {
+    return dayjs(date).format(`YYYY/MM/DD HH:mm`);
+  }
 };
--- src/view/comment.js
+++ src/view/comment.js
@@ -1,6 +1,7 @@
 import Abstract from './abstract';
-import dayjs from "dayjs";
 import he from "he";
+import {getFormattedTime} from '../utils';
+
 
 const createCommentTemplate = (comment) => {
   const {
@@ -9,54 +10,6 @@ const createCommentTemplate = (comment) => {
     date: commentDate,
     emotion
   } = comment;
-
-  const getFormattedTime = (date) => {
-
-    const difference = (+new Date() - +new Date(date)) / 60000;
-
-    const hoursDiff = difference / 60;
-
-    const daysDiff = hoursDiff / 24;
-
-    const monthDiff = daysDiff / 30;
-
-    if (difference < 1) {
-      return `now`;
-
-    } else if (difference >= 1 && difference < 5) {
-      return `a few minutes ago`;
-
-    } else if (difference >= 5 && difference < 60) {
-      return `${Math.floor(difference)} minutes ago`;
-
-    } else if (difference > 60 && difference < 61) {
-      return `1 hour ago`;
-
-    } else if (difference >= 61 && hoursDiff < 24) {
-      return `${Math.floor(hoursDiff)} hours ago`;
-
-    } else if (hoursDiff > 24 && hoursDiff < 48) {
-      return `1 day ago`;
-
-    } else if (hoursDiff >= 48 && daysDiff < 30) {
-      return `${Math.floor(daysDiff)} days ago`;
-
-    } else if (daysDiff >= 30 && daysDiff < 60) {
-      return `1 month ago`;
-
-    } else if (daysDiff >= 60 && monthDiff < 12) {
-      return `${Math.floor(monthDiff)} month ago`;
-
-    } else if (monthDiff >= 12 && monthDiff < 24) {
-      return `1 year ago`;
-
-    } else if (monthDiff > 24) {
-      return `${Math.floor(monthDiff / 12)} years ago`;
-
-    } else {
-      return dayjs(commentDate).format(`YYYY/MM/DD HH:mm`);
-    }
-  };
 
   return `<li class="film-details__comment">
   <span class="film-details__comment-emoji">
